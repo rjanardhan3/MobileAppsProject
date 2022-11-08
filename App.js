9875bf56caad945d16e3ddeddaead629219bd13e
rajbrady <raj@alan.app>
2022-11-08 01:22:11 -0500

    Recipe Card

--- App.js
+++ App.js
@@ -3,16 +3,30 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Button, FlatList, TouchableOpacity } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from "expo-font";
 import LoginScreen from './screens/LoginScreen';
 import FavoriteRecipes from './screens/FavoriteRecipes';
 import AddingIngredientScreen from './screens/AddingIngredientScreen';
 import HomeScreen from './screens/HomeScreen.js';
 import IngredientListScreen from './screens/IngredientListScreen';
 import TakePhotoScreen from './screens/TakePhotoScreen';
+import RecipeMenu from './screens/RecipeMenu';
+import RecipeInstructions from './screens/RecipeInstructions';
+import { COLORS, SIZES, SHADOWS, FONTS, assets } from "./constants/theme";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf")
+  });
+  if(!loaded) return null;
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
@@ -40,7 +54,17 @@ export default function App() {
           name="AddingIngredientScreen"
           component={AddingIngredientScreen}
         />
-
+        <Stack.Screen
+          name="RecipeMenu"
+          component={RecipeMenu}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="RecipeInstructions"
+          component={RecipeInstructions}
+          options={{ headerShown: false }}
+        />
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
