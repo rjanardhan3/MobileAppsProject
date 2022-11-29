34909799a8f318053f36e4da4d513444f55291bb
rajbrady <raj@alan.app>
2022-11-29 13:37:50 -0500

    Model Changes

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,6 +1,16 @@
 const { getDefaultConfig } = require("@expo/metro-config");
+module.exports = (async () => {
+  const defaultConfig= await defaultConfig();
+  const { assetExs } = defaultConfig.resolver;
+  return {
+    resolver: {
+      assetExs: [...assetExs, 'bin']
+    }
 
-const defaultConfig = getDefaultConfig(__dirname);
+  }
+})
+
+//const defaultConfig = getDefaultConfig(__dirname);
 
 defaultConfig.resolver.assetExts.push("cjs");
 
